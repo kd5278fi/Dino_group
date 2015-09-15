1a8e0d7cf9606518b6c9b62615816151d74313c4
Jensina Mart <jensinamart@Yosemites-MacBook-Pro.local>
2015-09-14 19:48:24 -0500

    work, damnit

--- Dinosaur.py
+++ Dinosaur.py
@@ -3,7 +3,7 @@ __author__ = 'Laura'
 class dinosaur(object):
     def roar(self):
 
-        print("Everybody do the dino.rar!")
+        print("Everybody do the dinorarrrr!")
 
         print("Blrblrblrblr!")
         print("argharghrgh")
