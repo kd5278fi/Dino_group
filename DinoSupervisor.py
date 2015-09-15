312681b54a060d677ea01a0f1427134e5a15ab50
Rom Stevens <romney.stevens@gmail.com>
2015-09-14 19:48:45 -0500

    Dinosaurs ROAR THEIR NAMES!!!

--- DinoSupervisor.py
+++ DinoSupervisor.py
@@ -4,8 +4,8 @@ from Dinosaur import dinosaur
 
 def main():
     import Dinosaur
-    t_rex=dinosaur()
-    velociraptor=dinosaur()
+    t_rex=dinosaur('t-rex')
+    velociraptor=dinosaur('velociraptor')
     t_rex.roar()
     velociraptor.roar()
 
--- Dinosaur.py
+++ Dinosaur.py
@@ -1,9 +1,8 @@
 __author__ = 'Laura'
 
 class dinosaur(object):
+    def __init__(self, name):
+        self.name = name
+
     def roar(self):
-
-        print("Everybody do the dinosaur!")
-
-        print("Blrblrblrblr!")
-        print("argharghrgh")
+        print "I AM " + self.name.upper() + "!!!"
