7a726f771a88d275b094433ab0a46d752c46085d
jensinamart <jensinamart@gmail.com>
2015-09-16 17:20:08 -0500

    I'm not afraid of commitment!

--- Dinosaur.py
+++ Dinosaur.py
@@ -3,7 +3,9 @@ __author__ = 'Laura'
 class dinosaur(object):
     def roar(self):
 
-        print("Everybody do the dinorarrr!")
+        print("Everybody do the dinorarr!")
+        #Do the thing
+
 
         print("Blrblrblrblr!")
         print("argharghrgh")
