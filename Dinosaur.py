8b8e93e0b2feff46246f1b88431f6bea81d716b4
kd5278fi <kd5278fi@go.minneapolis.edu>
2015-09-21 14:52:28 -0500

    Added weather affect and dinos reaction

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,9 +1,23 @@
 __author__ = 'Laura'
 
+from Weather import weather
+
 class dinosaur(object):
+    def __init__(self, status = "Fine"):
+        self.status = status
+
     def roar(self):
 
         print("Everybody do the dinosaur!")
 
         print("Blrblrblrblr!")
 
+    def react(self, weather_object):
+        if weather_object.stormy == True:
+            print("I am wet.")
+        elif weather_object.blizzard == True:
+            print("I am cold.")
+        elif weather_object.sunny == True:
+            print("I am warm.")
+        elif weather_object.meteor_shower == True:
+            print("I am dead.")
--- Weather.py
+++ Weather.py
@@ -1,1 +1,40 @@
 __author__ = 'Laura'
+
+from random import randint
+class weather(object):
+
+    def __init__(self, stormy = False, blizzard = False, sunny = False, meteor_shower = False):
+        self.stormy = stormy
+        self.blizzard = blizzard
+        self.sunny = sunny
+        self.meteor_shower = meteor_shower
+
+    def random_weather(self):
+        self.reset()
+        counter = randint(1,4)
+        if counter == 1:
+            self.stormy = True
+        elif counter == 2:
+            self.blizzard = True
+        elif counter == 3:
+            self.sunny = True
+        elif counter == 4:
+            self.meteor_shower = True
+
+    def reset(self):
+        self.stormy = False
+        self.blizzard = False
+        self.sunny = False
+        self.meteor_shower = False
+
+    def weatherman(self):
+        if self.stormy == True:
+            print("It's a dreary, stormy day.")
+        elif self.blizzard == True:
+            print("It's a cold, snowy day.")
+        elif self.sunny == True:
+            print("It's a beautiful, sunny day.")
+        elif self.meteor_shower == True:
+            print("It's the end of the world.")
+        else:
+            print("It's unremarkable today.")
